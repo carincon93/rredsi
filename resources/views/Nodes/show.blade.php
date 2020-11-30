61a2fea3097e9c2c45ce8bf1ce9471cf87cc1601
Cristian Rincón <carincon93@gmail.com>
2020-11-30 09:15:30 -0500

    Show modified

--- resources/views/Nodes/show.blade.php
+++ resources/views/Nodes/show.blade.php
@@ -1,40 +1,81 @@
-@extends('layouts.app')
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-display text-white text-3xl leading-9 font-semibold sm:text-3xl sm:leading-9">
+      {{ __('Nodes') }}
+      <span class="sm:block text-purple-300">
+        Show node info
+      </span>
+    </h2>
+    <div>
+      <a href="{{ route('nodes.update', [$node]) }}">
+        <div class="w-full sm:w-auto items-center justify-center text-purple-900 group-hover:text-purple-500 font-medium leading-none bg-white rounded-lg shadow-sm group-hover:shadow-lg py-3 px-5 border border-transparent transform group-hover:-translate-y-0.5 transition-all duration-150">
+          {{ __('Edit node') }}
+        </div>
+      </a>
+    </div>
+  </x-slot>
 
-@section('content')
-
-<div class="container">
-    <div class="card p-4 detail">
-      <div class="card-header">
-          <h4>{{ $node->state }}</h4>
-          <a href="/app/nodes/edit/{{ $node->id }}"> Editar </a>
+  <div>
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+      <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="md:col-span-1">
+          <h3 class="text-lg font-medium text-gray-900">Información del nodo <span class="text-capitalize">{{ $node->state }}</span></h3>
+        </div>
+        <div class="mt-5 md:mt-0 md:col-span-2">
+          <div class="px-4 py-5 sm:p-6 bg-white shadow sm:rounded-lg">
+            <h3 class="text-lg font-medium text-gray-900">{{ __('Name') }}</h3>
+            <div class="mt-3 max-w-xl text-sm text-gray-600">
+              <p>
+                {{ $node->administrator->name }}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-  <hr/>
-      <ul class="list-unstyled">
-        <li class="media">
-          <img class="mr-3" src="" alt="circle" />
-          <div class="media-body">
-              <h5 class="mt-0 mb-1">Administrador del nodo</h5>
-              {{ $node->administrator?->user->name }}
+
+      <div class="hidden sm:block">
+        <div class="py-8">
+            <div class="border-t border-gray-200"></div>
+        </div>
+      </div>
+
+      <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="md:col-span-1">
+          
+        </div>
+        <div class="mt-5 md:mt-0 md:col-span-2">
+          <div class="px-4 py-5 sm:p-6 bg-white shadow sm:rounded-lg">
+            <h3 class="text-lg font-medium text-gray-900">{{ __('Cellphone number') }}</h3>
+            <div class="mt-3 max-w-xl text-sm text-gray-600">
+              <p>
+                {{ $node->administrator->cellphone_number }}
+              </p>
+            </div>
           </div>
-        </li>
-        <li class="media my-4">
-          <img class="mr-3" src="" alt="circle" />
-          <div class="media-body">
-              <h5 class="mt-0 mb-1">Número de teléfono del administrador del nodo</h5>
-              {{ $node->administrator?->user->cellphone_number }}
+        </div>
+      </div>
+
+      <div class="hidden sm:block">
+        <div class="py-8">
+            <div class="border-t border-gray-200"></div>
+        </div>
+      </div>
+
+      <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="md:col-span-1">
+          
+        </div>
+        <div class="mt-5 md:mt-0 md:col-span-2">
+          <div class="px-4 py-5 sm:p-6 bg-white shadow sm:rounded-lg">
+            <h3 class="text-lg font-medium text-gray-900">{{ __('Email') }}</h3>
+            <div class="mt-3 max-w-xl text-sm text-gray-600">
+              <p>
+                {{ $node->administrator->email }}
+              </p>
+            </div>
           </div>
-        </li>
-        <li class="media my-4">
-          <img class="mr-3" src="" alt="circle" />
-          <div class="media-body">
-              <h5 class="mt-0 mb-1">Correo electrónico del administrador del nodo</h5>
-              {{ $node->administrator?->user->email }}
-          </div>
-        </li>
-      </ul>
+        </div>
+      </div>
     </div>
   </div>
-
-
-
-@endsection
+</x-app-layout>
