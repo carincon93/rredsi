bfbbcab1572c01b67ac621422d8c3a34d71847aa
DanielMG1 <danielmelangiraldo@gmail.com>
2021-05-04 22:31:06 -0500

    ajustes observatorio

--- resources/views/components/welcome-company.blade.php
+++ resources/views/components/welcome-company.blade.php
@@ -7,11 +7,7 @@
         Bienvenido a la aplicación {{ config('app.name') }}!
     </div>
 
-    <div class="mt-6 text-gray-500">
-        Con la plataforma {{ config('app.name') }} puede buscar proyectos de su
-        interés que se esten desarrollando en los semilleros de investigación de las 10
-        instituciones educativas de RREDSI nodos Caldas y Valle, colaborar en su desarrollo
-        o registrar una idea empresarial y conectarse con jóvenes investigadores
-        de dichas instituciones.
+    <div class="text-justify mt-6 text-gray-500 ">
+        Con la plataforma {{ config('app.name') }} puede buscar proyectos de su interés que se esten desarrollando en los semilleros de investigación de las 10 instituciones educativas de RREDSI nodos Caldas y Valle, colaborar en su desarrollo o registrar una idea empresarial y conectarse con jóvenes investigadores de dichas instituciones.
     </div>
 </div>
--- resources/views/dashboard-company.blade.php
+++ resources/views/dashboard-company.blade.php
@@ -15,17 +15,17 @@
                 <div class="ml-20 bg-white bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
                     <!-- Crea una card por cada uno de los projectos activos y -->
                     @foreach ($projects as $project)
-                        <div class="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden my-10">
+                        <div class="max-w-xs bg-gray-50 shadow-lg rounded-lg overflow-hidden my-10">
                             <div class="px-4 py-2">
-                                <h1 class="text-gray-900 font-bold text-3xl uppercase"><?php echo($project->title)?></h1>
-                                <p class="text-gray-600 text-sm mt-1">{{ substr($project->abstract, 0 ,310) }}@if(count_chars($project->abstract) > 310)...@endif</p>
+                                <h1 class="text-gray-900 font-bold text-lg mt-3"><?php echo($project->title)?></h1>
+                                <p class="text-gray-600 text-base mt-3">{{ substr($project->abstract, 0 ,250) }}@if(count_chars($project->abstract) > 250)...@endif</p>
                             </div>
                             <img class="h-56 w-full object-cover mt-2" src="https://image.freepik.com/vector-gratis/ilustracion-concepto-innovacion_114360-5578.jpg" alt="">
                             @php
                                 $researchTeam = $project->researchTeams()->where('is_principal', 1)->first() ?? null;
                             @endphp
                             <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
-                                <h1 class="text-gray-300 font-bold text-xl">{{ optional($researchTeam->researchGroup)->educationalInstitutionFaculty->educationalInstitution->name }}</h1>
+                                <h1 class="text-gray-300 font-bold">{{ optional($researchTeam->researchGroup)->educationalInstitutionFaculty->educationalInstitution->name }}</h1>
                                 <a href="#" class="px-3 py-1 bg-gray-200 text-sm text-gray-900 font-semibold rounded">Explorar</a>
                             </div>
                         </div>
